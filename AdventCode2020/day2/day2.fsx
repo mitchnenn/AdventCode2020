32a5183aeb4ce24cb05043b680fbd7e05854ade8
Mitch Nenn <mitch.nenn@earthlink.net>
2020-12-24 11:54:02 -0800

    day 2 part 1 complete

--- AdventCode2020/day2/day2.fsx
+++ AdventCode2020/day2/day2.fsx
@@ -1,44 +1,31 @@
-#r @"/Users/mnenn/.nuget/packages/unquote/5.0.0/lib/netstandard2.0/Unquote.dll"
-#r @"/Users/mnenn/.nuget/packages/fparsec/1.1.1/lib/netstandard2.0/FParsec.dll"
-#r @"/Users/mnenn/.nuget/packages/fparsec/1.1.1/lib/netstandard2.0/FParsecCS.dll"
+#r @"/Users/mnenn/.nuget/packages/fsharp.text.regexprovider/2.1.0/lib/netstandard2.0/FSharp.Text.RegexProvider.dll"
+open System.IO
+open FSharp.Text.RegexProvider
 
-open Swensen.Unquote
-open FParsec
+let path = Path.Combine($@"{__SOURCE_DIRECTORY__}", "input.txt")
+let input = File.ReadAllLines(path)
 
-let first (c, _, _) = c
-let second (_, c, _) = c
-let third (_, _, c) = c
+type PolicyPassword = { min:int; max:int; character:char; password:string }
 
-type UserState = unit // doesn't have to be unit, of course
-type Parser<'t> = Parser<'t, UserState>
+type PolicyPasswordRegex = Regex< @"^(?<Min>\d+)-(?<Max>\d+)\s(?<Character>[a-z]):\s(?<Password>.+)$" >
+// Test
+PolicyPasswordRegex().TypedMatch("7-12 h: hhhhhhdhhhhhfhhhh").Character.Value
 
-let testp p str =
-    match run p str with
-    | Success(result, _, _)   -> printfn "Success: %A" result
-    | Failure(errorMsg, _, _) -> printfn "Failure: %s" errorMsg
+let parsed =
+    input
+    |> Seq.map (PolicyPasswordRegex().TypedMatch)
+    |> Seq.map (fun m -> {min=int m.Min.Value;max=int m.Max.Value;character=char m.Character.Value;password=string m.Password.Value} )
+    |> Seq.toList
 
-testp pfloat "1.25"
+let isValidPassword (policyPassword:PolicyPassword) =
+    let occurrences = policyPassword.password
+                     |> Seq.filter (fun c -> c = policyPassword.character)
+                     |> Seq.length
+    occurrences >= policyPassword.min && occurrences <= policyPassword.max
 
-let path = System.IO.Path.Combine($@"{__SOURCE_DIRECTORY__}", "input.txt")
-
-type Policy = {Min:int; Max:int; C:char}
-type Password = {thePolicy:Policy; Password:string}
-
-let getMin : Parser<_> = pint32 .>> pchar '-'
-let getMax : Parser<_> = pint32 .>> spaces
-let getRange : Parser<_> = getMin .>>. getMax  
-let getChar : Parser<_> = anyChar .>> skipChar ':'
-
-let getPolicy (input:string) =
-    let result = run (tuple3 getMin getMax getChar) input
-    match result with
-    | Success(p,_,_) -> {Min=(first p);Max=(second p);C=(third p)}
-    | Failure(msg,_,_) -> failwith msg
+parsed
+|> List.filter (isValidPassword)
+|> List.length
 
 printf "Testing..."
-testp getMin "7-16 r: nvrrzhsgrwthgbrqkqh"
-testp (tuple2 getMin getMax) "7-16 r: nvrrzhsgrwthgbrqkqh"
-testp getRange "7-16 r: nvrrzhsgrwthgbrqkqh"
-testp (tuple3 getMin getMax getChar) "7-16 r: nvrrzhsgrwthgbrqkqh"
-getPolicy "7-16 r: nvrrzhsgrwthgbrqkqh"
 printfn "done!"
